5cc20c4a7ddb61498bacec69203e34af0a2b40f6
dark-slayers <23534878@qq.com>
2018-02-26 22:24:20 +0800

    删除无用代码

--- src/main/java/person/liuxx/movie/service/impl/RunService.java
+++ src/main/java/person/liuxx/movie/service/impl/RunService.java
@@ -1,19 +1,7 @@
 package person.liuxx.movie.service.impl;
 
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.time.LocalDate;
-
-import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.beanutils.BeanUtilsBean;
-import org.apache.commons.beanutils.MethodUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import person.liuxx.movie.domain.MovieDO;
-import person.liuxx.movie.dto.MovieDTO;
-import person.liuxx.util.log.LogUtil;
 
 /**
  * @author 刘湘湘
@@ -27,40 +15,7 @@ public class RunService
 
     public void run()
     {
-        MovieDO a = new MovieDO();
-
-        String name = "code";
-        a.setActress("ACTER");
-        a.setCode("000212");
-        a.setId(32L);
-        a.setLabel("LD");
-        a.setLevel(3);
-        a.setPath("D:/t");
-        log.debug("a", a);
-        MovieDTO ac =MovieDTO.of(a);
-        log.debug("MovieDO ac:{}", ac);
-//        try
-//        {
-//            PropertyDescriptor desc = BeanUtilsBean.getInstance()
-//                    .getPropertyUtils()
-//                    .getPropertyDescriptor(a, name);
-//            // getPropertyDescriptor(a, name);
-//            log.debug("desc:{}", desc);
-//            log.debug("desc.getReadMethod():{}", desc.getReadMethod());
-//            Method m = MethodUtils.getAccessibleMethod(MovieDO.class, desc.getReadMethod());
-//            System.out.println(m);
-//            Class<?> clazz = desc.getReadMethod().getDeclaringClass();
-//            log.debug("clazz:{}", clazz);
-//            boolean show = BeanUtilsBean.getInstance().getPropertyUtils().isReadable(a, name);
-//            boolean show2 = BeanUtilsBean.getInstance().getPropertyUtils().isWriteable(a, name);
-//            System.out.println(show);
-//            System.out.println(show2);
-//            BeanUtils.copyProperties(ac, a);
-//            log.debug("MovieDO ac:{}", ac);
-//        } catch (Exception e)
-//        {
-//            log.debug(LogUtil.errorInfo(e));
-//        }
+        log.debug("RunService:{}", "run");
     }
 
     /**
